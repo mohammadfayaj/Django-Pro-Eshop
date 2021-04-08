8c3ec86e265a162514b5f28da5dfbc53864b3f21
Mohammad Fayaj <61315077+mohammadfayaj@users.noreply.github.com>
2021-04-08 18:15:46 +0600

    Update settings.py

--- Project/settings.py
+++ Project/settings.py
@@ -5,7 +5,7 @@ from decouple import config
 import os
 
 
-DATABASES = { 'default': dj_database_url.config(conn_max_age=600, ssl_require=True) }
+DATABASES = { 'default': dj_database_url.config() }
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
