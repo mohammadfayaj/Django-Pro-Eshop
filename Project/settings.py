2a81b37d47c4475c1911d8d50dadc39e751094aa
Mohammad Fayaj <61315077+mohammadfayaj@users.noreply.github.com>
2021-04-08 18:14:54 +0600

    Update settings.py

--- Project/settings.py
+++ Project/settings.py
@@ -2,27 +2,10 @@ from pathlib import Path
 import django_heroku
 import dj_database_url
 from decouple import config
-import logging
 import os
 
 
 DATABASES = { 'default': dj_database_url.config(conn_max_age=600, ssl_require=True) }
-
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'WARNING',
-    },
-}
-
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
