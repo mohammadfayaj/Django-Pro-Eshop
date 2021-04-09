1d391f8be6d6dfa6ccda0f041d4642d4fbb0df59
Mohammad Fayaj <61315077+mohammadfayaj@users.noreply.github.com>
2021-04-09 18:46:00 +0600

    Update 0001_initial.py

--- shop/migrations/0001_initial.py
+++ shop/migrations/0001_initial.py
@@ -13,8 +13,9 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('users', '0001_initial'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+
     ]
 
     operations = [
